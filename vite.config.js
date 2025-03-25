97c98109e11eca906da27e40b0925a75f7fec151
User <user@example.com>
2025-03-25 15:00:32 -0700

    test

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/multiplicative_identity/',
+  base: '/roman_numerals/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
