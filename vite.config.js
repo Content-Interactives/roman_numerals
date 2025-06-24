cc09ee4afc29cd4b4d615738c16af7896ee74075
Destiny Nguyen <destinynguyen@Destinys-MacBook-Air-2.local>
2025-06-24 09:56:27 -0700

    added vite

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 8080,
-    open: true
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true
-  }
+  base: '/roman_numerals/'
 })
